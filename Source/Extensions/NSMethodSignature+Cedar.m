f43a0366230e0cf143ceac52512951a45a1bef91
David Post <david.post@replicon.com>
2025-12-18 12:26:14 -0700

    update

--- Source/Extensions/NSMethodSignature+Cedar.m
+++ Source/Extensions/NSMethodSignature+Cedar.m
@@ -12,41 +12,40 @@ static const char *Block_signature(id blockObj) {
     return signature;
 }
 
-static NSString *cdr_stripProblematicEncodings(const char *typeEncoding) {
+@implementation NSMethodSignature (Cedar)
+
++ (NSString *) cdr_stripProblematicEncodings:(const char *) typeEncoding {
     if (!typeEncoding) {
         return @"";
     }
-
+    
     NSString *typeEncodingString = [NSString stringWithUTF8String:typeEncoding];
-
+    
     // Replace ANY parenthesized union/struct encodings with 'B' (C++ bool)
     // This handles complex encodings like (?={?=CCCCCCCC}Q) in Xcode 26+
     // More aggressive pattern to catch all variants
     NSRegularExpression *unionPattern = [NSRegularExpression regularExpressionWithPattern:@"\\([^)]*\\)" options:0 error:NULL];
     NSString *strippedTypeEncoding = [unionPattern stringByReplacingMatchesInString:typeEncodingString options:0 range:NSMakeRange(0, [typeEncodingString length]) withTemplate:@"B"];
-
+    
     // Strip quoted substrings (e.g., "name")
     NSRegularExpression *quotedPattern = [NSRegularExpression regularExpressionWithPattern:@"\"[^\"]*\"" options:0 error:NULL];
     strippedTypeEncoding = [quotedPattern stringByReplacingMatchesInString:strippedTypeEncoding options:0 range:NSMakeRange(0, [strippedTypeEncoding length]) withTemplate:@""];
-
+    
     // Strip angle-bracketed content (e.g., <ProtocolName>)
     NSRegularExpression *angleBracketPattern = [NSRegularExpression regularExpressionWithPattern:@"<[^>]*>" options:0 error:NULL];
     strippedTypeEncoding = [angleBracketPattern stringByReplacingMatchesInString:strippedTypeEncoding options:0 range:NSMakeRange(0, [strippedTypeEncoding length]) withTemplate:@""];
-
+    
     // If after stripping we end up with an empty or whitespace-only string, default to 'B' (bool)
     strippedTypeEncoding = [strippedTypeEncoding stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     if ([strippedTypeEncoding length] == 0) {
         return @"B";
     }
-
+    
     return strippedTypeEncoding;
 }
-
-@implementation NSMethodSignature (Cedar)
-
 + (NSMethodSignature *)cdr_signatureFromBlock:(id)block {
     const char *signatureTypes = Block_signature(block);
-    NSString *strippedSignatureTypeString = cdr_stripProblematicEncodings(signatureTypes);
+    NSString *strippedSignatureTypeString = [self cdr_stripProblematicEncodings: signatureTypes];
     return [NSMethodSignature signatureWithObjCTypes:[strippedSignatureTypeString UTF8String]];
 }
 
@@ -61,13 +60,13 @@ static NSString *cdr_stripProblematicEncodings(const char *typeEncoding) {
 
         // Add the return type
         const char *returnType = [signature methodReturnType];
-        NSString *cleanedReturnType = cdr_stripProblematicEncodings(returnType);
+        NSString *cleanedReturnType = [self cdr_stripProblematicEncodings: returnType];
         [cleanedSignatureString appendString:cleanedReturnType];
 
         // Add all argument types
         for (NSUInteger i = 0; i < [signature numberOfArguments]; i++) {
             const char *argType = [signature getArgumentTypeAtIndex:i];
-            NSString *cleanedArgType = cdr_stripProblematicEncodings(argType);
+            NSString *cleanedArgType = [self cdr_stripProblematicEncodings: argType];
             [cleanedSignatureString appendString:cleanedArgType];
         }
 
@@ -84,11 +83,12 @@ static NSString *cdr_stripProblematicEncodings(const char *typeEncoding) {
     NSAssert([self numberOfArguments]>1 && strcmp([self getArgumentTypeAtIndex:1], ":")==0, @"Unable to remove _cmd from a method signature without a _cmd argument");
 
     NSMutableString *modifiedTypesString = [NSMutableString string];
-    [modifiedTypesString appendString:cdr_stripProblematicEncodings([self methodReturnType])];
+    
+    [modifiedTypesString appendString:[NSMethodSignature cdr_stripProblematicEncodings: [self methodReturnType]]];
 
     for (NSInteger argIndex=0; argIndex<[self numberOfArguments]; argIndex++) {
         if (argIndex==1) { continue; }
-        [modifiedTypesString appendString:cdr_stripProblematicEncodings([self getArgumentTypeAtIndex:argIndex])];
+        [modifiedTypesString appendString:[NSMethodSignature cdr_stripProblematicEncodings: [self getArgumentTypeAtIndex:argIndex]]];
     }
 
     return [NSMethodSignature signatureWithObjCTypes:[modifiedTypesString UTF8String]];
--- Source/Headers/Project/Extensions/NSMethodSignature+Cedar.h
+++ Source/Headers/Project/Extensions/NSMethodSignature+Cedar.h
@@ -1,10 +1,11 @@
 #import <Foundation/Foundation.h>
 
 // Helper function to strip problematic type encodings
-NSString *cdr_stripProblematicEncodings(const char *typeEncoding);
+
 
 @interface NSMethodSignature (Cedar)
 
++ (NSString *) cdr_stripProblematicEncodings:(const char *)typeEncoding;
 + (NSMethodSignature *)cdr_signatureFromBlock:(id)block;
 + (NSMethodSignature *)cdr_sanitizedSignatureFromSignature:(NSMethodSignature *)signature;
 - (NSMethodSignature *)cdr_signatureWithoutSelectorArgument;
